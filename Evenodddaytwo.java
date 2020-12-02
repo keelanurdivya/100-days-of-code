2c95c852d4903838f083826e0c56b552135c8543
Keelanur Divya <52031269+keelanurdivya@users.noreply.github.com>
2020-12-02 23:49:58 +0530

    Add files via upload

---
+++ Evenodddaytwo.java
@@ -0,0 +1,32 @@
+public class Evenodddaytwo
+    {
+     public static void main(String []args)
+     {
+       int n=5;
+       
+     if(n%2==0)
+      {
+       
+        if(n>=2 && n<=5)
+         {
+          System.out.println("n is not wierd");
+         }
+
+         else if(n>=6 && n<=20)
+         {
+          System.out.println("n is wierd");
+         }
+
+           else if(n>20)
+            {
+             System.out.println("n is not wierd");
+            }
+
+    }
+      else
+      {
+       System.out.println("n is wierd");
+      }
+
+  }
+}
