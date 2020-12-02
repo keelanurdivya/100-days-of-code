55f5760f776dea1d7a1e51b565ae5084f2f77e6d
Keelanur Divya <52031269+keelanurdivya@users.noreply.github.com>
2020-12-02 20:16:44 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,34 @@
+public class Evenodd{
+
+     public static void main(String []args)
+     {
+         int n=5,i;
+         if(n%2==1)
+         {
+            System.out.println("n is wierd");
+         }
+         if(n%2==0)
+         {
+             for(i=2;i<=5;i++)
+             {
+                 System.out.println("n is not wierd");
+             }
+             else if
+              {
+                 for(i=6;i<=20;i++)
+                 {
+                     System.out.println("n is wierd");
+                 }
+             
+                 else
+                 {
+                   for(i=20;i>20;i++)
+                    {
+                     System.out.println("n is not wierd");
+                    }  
+                 }
+              }
+           }
+        
+     }
+}
