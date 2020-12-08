f0497280a37cb549fdcb70e1d8aefa6d2454f753
Keelanur Divya <52031269+keelanurdivya@users.noreply.github.com>
2020-12-08 23:59:06 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class factorial
+{
+public static void main(String args[])
+{
+int n=5,i,fact=1;
+for(i=1;i<=n;i++)
+{
+  fact=fact*i;
+}
+System.out.print("Factorial of 5 is"+fact);
+}
+}
+
+
+
+
---
+++ prime.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class prime
+{
+public static void main(String args[])
+{
+ int n=21;
+ if(n/2==0)
+{
+ System.out.println("Not a prime");
+}
+else
+{
+  System.out.println("Its a prime");
+}
+}
+}
